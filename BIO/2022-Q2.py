5ece999863d4d38642bde03a6d0db69b1cbc94dc
RuthlessLama42 <111354054+RuthlessLama42@users.noreply.github.com>
2023-03-05 16:25:44 +0000

    Update 2022-Q2.py

--- BIO/2022-Q2.py
+++ BIO/2022-Q2.py
@@ -9,36 +9,58 @@ class hex():
 class hive():
     def __init__(self):
         self.hexagons = []
-        for i in range(27):  #number of hexagons
+        self.rows_list = []
+        for row in range(5):
             hex_ = hex()
             self.hexagons.append(hex_)
+            self.rows_list.append(self.hexagons)
     
     def __str__(self):
         output = ""
-        for n in self.hexagons:
-            value = n.sides
-            for i in value:
-                output = output + " " + i
+        for r in self.rows_list:
+            for n in self.hexagons:
+                value = n.sides
+                for i in value:
+                    output = output + " " + i
+                output = output + "     "
             output = output + "\n"
         return output
 
 class drone():
-    def __init__(self, pos, direct):
-        self.position = pos
+    def __init__(self, pos, direct, move):
+        self.position_row = pos[0]
+        self.position_hexagon = pos[1]
         self.direction = direct
+        self.move_distance = int(move)
 
-    def delete(self):
-        print("Hello, my position is",self.position)
-    
     def __str__(self):
-        outstring = str(self.position) + ", " + str(self.direction)
+        outstring = str(self.position_row) + ", " + str(self.position_hexagon) + " dir " + str(self.direction)
         return outstring
+
+    def move(self):
+
+        self.position_row =+ self.move_distance // 5
+        self.position_hexagon =+ self.move_distance % 5
+
+        if self.position_hexagon > 4:
+
+            self.position_hexagon -= 5
+            self.position_row += 12
+        
+        if self.position_row > 4:
+            self.position_row -= 5
+
+#_________________________________________________________________________________________________________________________________
+
+RED = input("RED MOVES between 1 and 25:")
+BLUE = input("BLUE MOVES between 1 and 25:")
 
 hive_ = hive()
 print(hive_)
 
-red_drone = drone(0, 0)
+red_drone = drone([0, 0], 0, RED)
+red_drone.move()
 print(red_drone)
-blue_drone = drone(27, 5)
+
+blue_drone = drone([4, 4], 5, BLUE)
 print(blue_drone)
-blue_drone.delete()
